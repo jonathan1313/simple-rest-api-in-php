f4bb069edb903f1c0daf7626a963bb1c171bcbd6
tda13 <jonathanwillianevo@hotmail.com>
2018-02-09 17:41:24 -0300

    Criada função de deletar produtos e iniciada função de procurar produtos

--- objects/product.php
+++ objects/product.php
@@ -65,6 +65,7 @@ class Product {
 	}
 
 	function readOne() {
+
 		$query = "SELECT
 					c.name AS category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
 				FROM
@@ -88,6 +89,65 @@ class Product {
 		$this->description = $row['description'];
 		$this->category_id = $row['category_id'];
 		$this->category_name = $row['category_name'];
+
+	}
+
+	function update() {
+
+		$query = "UPDATE
+					" . $this->table_name . "
+				SET
+					name = :name,
+					price = :price,
+					description = :description,
+					category_id = :category_id
+				WHERE
+					id = :id";
+
+		$stmt = $this->conn->prepare($query);
+
+		$this->name=htmlspecialchars(strip_tags($this->name));
+		$this->price=htmlspecialchars(strip_tags($this->price));
+		$this->description=htmlspecialchars(strip_tags($this->description));
+		$this->category_id=htmlspecialchars(strip_tags($this->category_id));
+		$this->id=htmlspecialchars(strip_tags($this->id));
+
+		$stmt->bindParam(':name', $this->name);
+		$stmt->bindParam(':price', $this->price);
+		$stmt->bindParam(':description', $this->description);
+		$stmt->bindParam(':category_id', $this->category_id);
+		$stmt->bindParam(':id', $this->id);
+
+		if ($stmt->execute()) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	function delete() {
+
+		$query = "DELETE FROM " . $this->table_name . "WHERE id = ?";
+
+		$stmt = $this->conn->prepare($query);
+
+		$this->id=htmlspecialchars(strip_tags($this->id));
+
+		$stmt->bindParam(1, $this->id);
+
+		if ($stmt->execute()) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+	function search($keywords) {
+
+						
+
 	}
 
 	
