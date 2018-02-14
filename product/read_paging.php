d458f14bfddad928312ddaedb62339e9efd7a336
tda13 <jonathanwillianevo@hotmail.com>
2018-02-14 17:35:15 -0300

    Concluida função de search e criado arquivos de read paging

---
+++ product/read_paging.php
@@ -0,0 +1,44 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/core.php'
+include_once '../shared/utilities.php'
+include_once '../config/database.php'
+include_once '../objects/product.php'
+
+$utilities = new Utilities();
+
+$database = new Database();
+$db = $database->getConnection();
+
+$product = new Product($db);
+
+$stmt = $product->readPaging($from_record_num, $records_per_page);
+$num = $stmt->rowCount();
+
+if ($num>0) {
+
+	$products_arr=array();
+	$products_arr["records"]=array();
+	$products_arr["paging"]=array();
+
+	while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+		extract($row);
+
+		$products_item=array(
+			"id" => $id,
+			"name" => $name,
+			"description" => html_entity_decode($description),
+			"category_id" => $category_id,
+			"category_name" => $category_name
+		);
+
+		array_push($products_arr["records"], $products_item);
+	}
+
+} 
+else {
+	# code...
+}
